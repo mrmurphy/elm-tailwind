9c2ac4432de774002fc58ea3e34cdf7593541089
Murphy Randle <murphyrandle@gmail.com>
2017-11-21 20:54:11 -0700

    Initial

---
+++ src/Tailwind.elm
@@ -0,0 +1,61 @@
+module Tailwind exposing (..)
+
+{-| A small library that makes using [tailwind.css](https://tailwindcss.com/) a little easier and a little safer to use within Elm.
+
+@docs stylesheet, classes
+
+This is mostly just a placeholder for the library. The goal would be to write a script that can generate an API for the tailwind library. A basic step would be to generate functions for all of the possible classes, and that helps avoid spelling errors and makes it easier to find usages of a class. But a more ideal solution would be to make a more type-safe api, maybe something like this:
+
+To give types to: <https://tailwindcss.com/docs/spacing/#app>
+
+    spacing : SpacingFlavor -> Side -> SpacingLength -> String
+
+    type SpacingFlavor
+        = Padding
+        | Margin
+        | NegMargin
+
+    type Side
+        = All
+        | Top
+        | Left
+        | Right
+        | Bottom
+        | X
+        | Y
+
+    type SpacingLength
+        = S0
+        | S1
+        | S2
+        | S3
+        | S4
+        | S5
+        | S6
+        | S7
+        | S8
+        | Spx
+        | Sauto
+
+And a vertical padding could be expressed like so:
+
+`padding Y S2`
+
+-}
+
+import Html
+import Html.Attributes exposing (href, rel)
+
+
+{-| An HTML "link" tag that will import the default tailwind stylesheet
+-}
+stylesheet : Html.Html msg
+stylesheet =
+    Html.node "link" [ rel "stylesheet", href "https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" ] []
+
+
+{-| A convenience function for specifying classes as a list of strings
+-}
+classes : List String -> Html.Attribute msg
+classes cs =
+    Html.Attributes.class <| String.join " " cs
